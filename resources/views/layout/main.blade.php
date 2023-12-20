49922d740798af5761b476c22e3f0a06c9168b92
Vicente Piltcher <vipiltcher62@gmail.com>
2023-12-20 14:29:33 -0300

    some updates and bugs fixed

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,37 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ Vite::asset('resources/images/favicon.png') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
 
-<header>
-    <nav style="width: 40%; background: #184BFF;">
-        <a href="/#"><img style="width: 55px; height: 55px;" href="rodrigopiltcher.test/favicon.png"></a>
-        <div class="div1nav">
-            <a class="logo" href="/#">Rodrigo Piltcher</a>
-            <p class="subtitle" >Aparelho Digestivo e Cirurgia Robótica</p>
-        </div>
-    </nav>
-</header>
+<div style="display: inline; width: 100%">
+    <div>   
+        <nav style="padding-left: 200px;">
+            <a href="/#" id="logo"><img style="height: 55px; width: 55px;" src="{{ Vite::asset('resources/images/favicon.png') }}"></a>
+            <div class="div1nav">
+                <a class="title" href="/#">Rodrigo Piltcher</a>
+                <p class="subtitle" >Aparelho Digestivo e Cirurgia Robótica</p>
+            </div>
+        </nav>
+    </div>
+</div>
 
+<main style="border: 1px solid red">
+    @yield('fotos-passando')
+</main>
 
-<!--
-<header>
-    <nav style="width: 100%">
-        <img href="{{ asset('favicon.png') }}">
-        <a class="logo" href="/#">Rodrigo Piltcher</a>
-        <ul class="nav-list">
-            <li><a href="/#">Início</a></li>
-            <li><a href="/#">Especialidades</a></li>
-            <li><a href="/#">Cirurgias</a></li>
-            <li><a href="/#">Contato</a></li>
-            <li><a href="/#"><img href="{{ asset('vendor/img/instagram.png') }}"></a></li>
-            <li><a href="/#"><img href="{{ asset('vendor/img/face.png') }}"></a></li>
-        </ul>
-    </nav>
-</header> -->
+<footer>
+    <div class="footerdiv">
+        <p>© 2023 Doutor Rodrigo Piltcher. Todos os direitos reservados.
+        <br/>CRM-RS: XXXXX</p>
+    </div>
+</footer>
 
 <style>
     * {
@@ -43,11 +40,27 @@
     }
 
 /* CSS DA PRIMEIRA NAV BAR (AZUL ESCURO) */
+    body{
+        overflow-y: visible;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    .footerdiv p{
+        font-size: 11px;
+        line-height: normal;
+        color: #fff;
+    }
+
     .div1nav{
         padding-left: 40px;       
     }
 
-    .logo{
+    #logo{
+        padding-top:5px;
+    }
+
+    .title{
         font-size: 32px;
         font-style: italic;
         font-weight: 600;
@@ -82,7 +95,22 @@
         align-items:  center;
         height: 70px;
         align-items: flex-start;
+        background: #184BFF;
     }
+
+    footer{
+        background: #184BFF;
+        position: absolute;
+        bottom:0;
+        width:100%;
+        height: 50px;
+        padding: 10px 5px;
+        margin-bottom: 0; /* reduzi a margem inferior para 0 */
+
+        text-align: center;
+    }
+
+    @yield('style')
 </style>
 
 </body>
--- resources/views/mainView.blade.php
+++ resources/views/mainView.blade.php
@@ -2,6 +2,29 @@
 
 @section('title', 'Rodrigo Piltcher')
 
-@section('content')
+@section('fotos-passando')
 
+    <div class="container-fluid" style="border: 1px solid blue">
+        <div id="mainSlider" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#mainSlider" data-slide-to="0" class="active"></li>
+                <li data-target="#mainSlider" data-slide-to="1"></li>
+                <li data-target="#mainSlider" data-slide-to="2"></li>
+                <li data-target="#mainSlider" data-slide-to="3"></li>
+                <li data-target="#mainSlider" data-slide-to="4"></li>
+                <li data-target="#mainSlider" data-slide-to="5"></li>
+            </ol>
+
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100 " src="{{ Vite::asset('resources/images/fotospassando/img5') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('style')
+    
 @endsection
